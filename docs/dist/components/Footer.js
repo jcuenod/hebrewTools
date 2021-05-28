4e4e7d76cd557528581d008fec04a2b3ddaaec57
James Cuénod <jcuenod@users.noreply.github.com>
2021-05-28 11:34:54 -0500

    Update build

--- docs/dist/components/Footer.js
+++ docs/dist/components/Footer.js
@@ -4,12 +4,16 @@ const Link = ({url, children}) => /* @__PURE__ */ React.createElement("a", {
   href: url
 }, children);
 const Footer = () => /* @__PURE__ */ React.createElement("div", {
-  className: "flex flex-row p-1 mt-2 justify-center bg-gray-200 text-sm text-gray-900"
-}, "Made with ♥ by ", /* @__PURE__ */ React.createElement(Link, {
+  className: "flex flex-row p-2 mt-2 justify-center bg-gray-200 text-xs text-gray-900"
+}, /* @__PURE__ */ React.createElement("span", {
+  className: "text-center"
+}, "Made with ", /* @__PURE__ */ React.createElement("span", {
+  className: "text-red-500"
+}, "♥"), " by ", /* @__PURE__ */ React.createElement(Link, {
   url: "https://jcuenod.github.io/"
 }, "James Cuénod"), ". Want to contribute:", /* @__PURE__ */ React.createElement(Link, {
   url: "https://github.com/jcuenod/hebrewTools"
 }, "code"), "/", /* @__PURE__ */ React.createElement(Link, {
   url: "https://donorbox.org/support-parabible"
-}, "love"));
+}, "money")));
 export default Footer;
--- docs/dist/components/Header.js
+++ docs/dist/components/Header.js
@@ -12,9 +12,9 @@ const Header = () => /* @__PURE__ */ React.createElement("div", {
   d: "M3.185.186a1.99 1.99 0 00-.057 0 1.99 1.99 0 00-.948.266 1.99 1.99 0 00-.729 2.72 1.99 1.99 0 00.04.066l1.69 2.926.695-1.205-.465-.806a1.99 1.99 0 00.76-.252 1.99 1.99 0 00.728-2.72A1.99 1.99 0 003.185.186zm-.029 1.209a.782.782 0 01.696.39.782.782 0 01-.286 1.069.782.782 0 01-1.068-.287.782.782 0 01.286-1.068.782.782 0 01.372-.104z",
   fill: "#0078d7"
 }))), /* @__PURE__ */ React.createElement("div", {
-  className: "p-2 flex-1 font-bold text-xl text-gray-700"
+  className: "p-2 flex-1 font-bold text-md sm:text-xl text-gray-700"
 }, "Hebrew Tools"), /* @__PURE__ */ React.createElement("a", {
-  className: "py-2 px-4 rounded-lg font-bold hover:bg-gray-200 text-gray-700 hover:text-blue-600",
+  className: "py-2 px-4 flex items-center rounded-lg text-sm sm:text-lg font-bold hover:bg-gray-200 text-gray-700 hover:text-blue-600",
   href: "https://parabible.com"
-}, "Try out parabible.com"));
+}, "Try parabible.com"));
 export default Header;

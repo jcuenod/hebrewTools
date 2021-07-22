6da6ce3fc603fbbcc9eef71b0839616e68c50e4c
James Cuénod <jcuenod@users.noreply.github.com>
2021-07-22 13:39:44 -0500

    Build with .jekyll (for gh-pages)

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
+    public: { url: '/', static: true, dot: true },
     src: '/dist',
     /* ... */
   },
